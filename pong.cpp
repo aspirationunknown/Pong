bd4c6d563ed272c724cbcdb2bbe80a4a3af73477
aspirationunknown <charles.parsons@mines.sdsmt.edu>
2014-09-15 14:20:39 -0600

    Added function names to be completed and comments on how some of those functions should work.

---
+++ pong.cpp
@@ -0,0 +1,63 @@
+
+void drawMenuText(menu)
+{
+
+
+}
+
+
+void applyCollision(ball b, paddle p)
+{
+    //determine if ball and paddle collide
+
+    //use ball position and diameter, paddle position and dimensions to
+    //determine collision.
+
+    //apply appropriate vector change to ball based on where on the paddle
+    //it collided.
+
+    //if ball hits top third of paddle make y velocity positive.
+    //if ball hits middle third of paddle do not change y velocity.
+    //if ball hits bottom third of paddle make y velocity negative.
+
+    //apply appropriate vector change to ball if it hits a wall.
+
+}
+
+
+void endGame(player winner)
+{
+
+
+
+}
+
+
+void drawBall()
+{
+
+
+}
+
+
+void drawPaddle()
+{
+    //can use glRect{sfid}(x1, y1, x2, y2)
+    //determine (x1, y1) & (x2, y2) using position and dimensions.
+
+}
+
+
+void drawField()
+{
+
+
+
+}
+
+
+void movePaddle(paddle p)
+{
+    //after keypress move paddle in the direction indicated by the key pressed.
+    
+}
