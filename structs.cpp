96d7d61b685cc7dc0a07b7cf08d2fd3714bcff86
Charles Parsons <cparsons@localhost.localdomain>
2014-09-30 15:25:36 -0600

    removed commented out code

--- structs.cpp
+++ structs.cpp
@@ -5,7 +5,7 @@
  * This file contains functionality for the structures in the pong game.
  ******************************************************************************/
 #include "structs.h"
-#include <iostream> 
+
 
  /***************************************************************************//**
  * applyCollision
@@ -44,42 +44,12 @@ bool applyCollision(Ball &ball, Paddle paddle[2])
     paddle_bounding_box[PLAYER_TWO][3] = paddle[PLAYER_TWO].position.second - 0.04 * //bottom-y
                                          paddle[PLAYER_TWO].dimensions.second;
 
-
-
     //use ball position(center) and diameter, paddle position(lower left-hand 
     //corner) and dimensions to determine collision.
 
     //if paddle is moving predict where paddle will be at next step and where
     //ball will be next step. if they would have passed through each other
     //cause collision.
-    /*if(paddle[PLAYER_ONE].velocity_vector.first != 0)
-    {
-        
-        //predict paddle's next location
-        //predict ball's next location
-        //if there should be a collision 
-        paddle_next_location = paddle[PLAYER_ONE].position.first + 
-                               paddle[PLAYER_ONE].velocity_vector.first;
-        ball_next_location = ball.position.first + ball.velocity_vector.first;
-        
-        if((ball_next_location - ball.diameter / 2.0 < 
-            paddle_next_location + 
-            paddle[PLAYER_ONE].dimensions.first) && 
-           (ball.position.second - ball.diameter / 2.0 > 
-            paddle[PLAYER_ONE].position.second - paddle[PLAYER_ONE].dimension.second * 0.1) &&
-           (ball.position.second + ball.diameter / 2.0 <
-            paddle[PLAYER_ONE].position.second + 
-            paddle[PLAYER_ONE].dimensions.second * 1.1) &&
-           (ball.position.first - ball.diameter / 2.0 > //checks if ball is not
-            paddle[PLAYER_ONE].position.first +
-            paddle[PLAYER_ONE].velocity_vector.first))         //behind paddle
-        {
-            ball.velocity_vector.first *= -1;
-            ball.position.first += 0.5 * ball.velocity_vector.first;
-            applySpin(ball, paddle[PLAYER_ONE]);
-            return true;
-        }
-        */
         if((ball_bounding_box[0] <= paddle_bounding_box[PLAYER_ONE][2]) &&
            (ball_bounding_box[3] <= paddle_bounding_box[PLAYER_ONE][1]) &&
            (ball_bounding_box[1] >= paddle_bounding_box[PLAYER_ONE][3]) &&
@@ -90,79 +60,7 @@ bool applyCollision(Ball &ball, Paddle paddle[2])
             applySpin(ball, paddle[PLAYER_ONE]);
             return true;
         }
-    /*}
-    else
-    {
-        //player1 collision: paddle right x pos is > ball x pos - diameter/2
-        //&& ball y pos +- diameter/2 is in between paddle y pos and y pos - 
-        //length
-        if((ball.position.first - ball.diameter / 2.0 < 
-            paddle[PLAYER_ONE].position.first + 
-            paddle[PLAYER_ONE].dimensions.first) && 
-           (ball.position.second - ball.diameter / 2.0 > 
-            paddle[PLAYER_ONE].position.second - paddle[PLAYER_ONE].dimension.second * 0.1) &&
-           (ball.position.second + ball.diameter / 2.0 <
-            paddle[PLAYER_ONE].position.second + 
-            paddle[PLAYER_ONE].dimensions.second * 1.1) &&
-           (ball.position.first - ball.diameter / 2.0 > //checks if ball is not
-            paddle[PLAYER_ONE].position.first))         //behind paddle
-        {
-            ball.velocity_vector.first *= -1;
-            ball.position.first += 0.5 * ball.velocity_vector.first;
-            applySpin(ball, paddle[PLAYER_ONE]);
-            return true;
-        }
-    }*/
-    
- /*   if(paddle[PLAYER_TWO].velocity_vector.first != 0)
-    {
-        //predict paddle's next location
-        //predict ball's next location
-        //if there should be a collision 
-        paddle_next_location = paddle[PLAYER_TWO].position.first + 
-                               paddle[PLAYER_TWO].velocity_vector.first;
-        ball_next_location = ball.position.first + ball.velocity_vector.first;
-        
-        if((ball_next_location + ball.diameter / 2.0 >
-            paddle_next_location) &&
-           (ball.position.second - ball.diameter / 2.0 > 
-            paddle[PLAYER_TWO].position.second - paddle[PLAYER_TWO].dimension.second * 0.1) &&
-           (ball.position.second + ball.diameter / 2.0 <
-            paddle[PLAYER_TWO].position.second +
-            paddle[PLAYER_TWO].dimensions.second * 1.1) &&
-           (ball.position.first + ball.diameter / 2.0 < //makes sure ball is not
-            paddle[PLAYER_TWO].position.first -
-            paddle[PLAYER_TWO].velocity_vector.first +         //behind paddle
-            paddle[PLAYER_TWO].dimensions.first))
-        {
-            ball.velocity_vector.first *= -1;
-            ball.position.first -= 0.5 * ball.velocity_vector.first;
-            applySpin(ball, paddle[PLAYER_TWO]);
-            return true;
-        }
-    }
-    else
-    {
-        //player2 collision: paddle x pos  is < ball x pos + diameter/2
-        //&& ball y pos +- diameter/2 is in between paddle y pos and y pos - 
-        //length
-        if((ball.position.first + ball.diameter / 2.0 >
-            paddle[PLAYER_TWO].position.first) &&
-           (ball.position.second - ball.diameter / 2.0 > 
-            paddle[PLAYER_TWO].position.second) &&
-           (ball.position.second + ball.diameter / 2.0 <
-            paddle[PLAYER_TWO].position.second +
-            paddle[PLAYER_TWO].dimensions.second) &&
-           (ball.position.first + ball.diameter / 2.0 < //makes sure ball is not
-            paddle[PLAYER_TWO].position.first +         //behind paddle
-            paddle[PLAYER_TWO].dimensions.first))
-        {
-            ball.velocity_vector.first *= -1;
-            ball.position.first -= 0.5 * ball.velocity_vector.first;
-            applySpin(ball, paddle[PLAYER_TWO]);
-            return true;
-        }
-    }*/
+
         if((ball_bounding_box[2] >= paddle_bounding_box[PLAYER_TWO][0]) &&
            (ball_bounding_box[3] <= paddle_bounding_box[PLAYER_TWO][1]) &&
            (ball_bounding_box[1] >= paddle_bounding_box[PLAYER_TWO][3]) &&
@@ -174,24 +72,6 @@ bool applyCollision(Ball &ball, Paddle paddle[2])
             return true;
         }
     return false;
-}
-
-
- /***************************************************************************//**
- * movePaddle
- * Authors - Derek Stotz, Charles Parsons
- *
- * Checks currently pressed keys and moves a given paddle based on which keys
-    are currently being pressed for a given player.
- ******************************************************************************/
-void movePaddle(Paddle paddle, player mover)
-{
-    //after key-press move paddle in the direction indicated by the key pressed.
-    //How does it know what key was pressed?
-    //if player1 use 'w', 'a', 's', 'd' keys for paddle movement.
-    //if player2 use 'up', 'down' ,'left', 'right' arrow keys for paddle 
-    //movement.
-    
 }
 
 
@@ -237,34 +117,12 @@ void applySpin(Ball &ball, Paddle &paddle)
 
 }
 
-/*Ball::Ball(int diam, int max_v, std::pair<int, int> v_vector, std::pair<int, int> pos, float* clr)
-{
-    this.diameter = diam;
-    this.max_velocity = max_v;
-    this.velocity_vector = v_vector;
-    this.position = pos;
-    this.color = clr;
-}
-
-Paddle::Paddle(std::pair<int, int> dim, std::pair<int, int> v_vector, std::pair<int, int> pos, std::pair<int, int> mov_speed, float* clr)
-{
-    this.dimensions = dim;
-    this.velocity_vector = v_vector;
-    this.position = pos;
-    this.movement_speed = mov_speed;
-    this.color = clr;
-}
-
-Menu::Menu(std::string* opt, int sel_index, fptr* opt_actions, float* bck_color, float* txt_color, float* sel_color)
-{
-    this.options = opt;
-    this.selection_index = sel_index;
-    this.option_actions = opt_actions;
-    this.background_color = bck_color;
-    this.text_color = txt_color;
-    this.selection_color = sel_color
-}*/
-
+ /***************************************************************************//**
+ * assignColor
+ * Authors - Derek Stotz, Charles Parsons
+ *
+ * Allows color changes from the globally declared colors.
+******************************************************************************/
 void assignColor(float value[3], const float color[3])
 {
     value[0] = color[0];
